0919cd9a09f2aee7e61a0d320eeb5eeefbbf10fd
Akshay Mohan <akshay.mohan@sensiaglobal.com>
2023-01-09 10:06:08 +0800

    Added some changes

--- getQuotes.py
+++ getQuotes.py
@@ -4,17 +4,19 @@
 import requests
 
 def getRandomQuote():
-	# Random Quote Generator
-	# https://github.com/lukePeavey/quotable
-	resp = requests.get('http://api.quotable.io/random')
-	if resp.status_code != 200:
-	# Throws an exception out
-	    raise requests.RequestException('GET /random/ {}'.format(resp.status_code))
-	# print(resp.json())
+    # Random Quote Generator
+    # https://github.com/lukePeavey/quotable
+    resp = requests.get('http://api.quotable.io/random')
+    if resp.status_code != 200:
+        # Throws an exception out
+        raise requests.RequestException(
+            'GET /random/ {}'.format(resp.status_code))
+    print(resp.json())
 
-	response = resp.json()
-	author = response['author']
-	quote = response['quote']
-	# ternary operator
-	output = quote+'\n-- '+author if (author != '') else quote
-	return output
+    response = resp.json()
+    author = response['author']
+    quote = response['quote']
+    # ternary operator
+    output = quote+'\n-- '+author if (author != '') else quote
+    print('output : '+output)
+    return output
