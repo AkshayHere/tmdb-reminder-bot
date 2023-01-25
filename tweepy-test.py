27cd5309fe0cdd800124ae9b00e8df4e79595e9d
Akshay Mohan <akshay.mohan@sensiaglobal.com>
2023-01-25 10:59:14 +0800

    Merge from stash

--- tweepy-test.py
+++ tweepy-test.py
@@ -2,6 +2,7 @@
 # Docs : https://github.com/tweepy/tweepy
 from dotenv import load_dotenv
 import tweepy
+import datetime
 
 import os
 from operator import attrgetter
@@ -22,12 +23,13 @@ access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 bearer_token = os.getenv("BEARER_TOKEN")
 CLIENT_KEY = os.getenv("CLIENT_KEY")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+USER_ID = 1523585758510010368  # TESTING USER ID
 
 
 def createQuoteTweet():
     print("Entering createQuoteTweet...")
-    # auth = tweepy.OAuth1UserHandler(consumer_key,
-    #                     consumer_secret, access_token, access_token_secret)
+    auth = tweepy.OAuth1UserHandler(consumer_key,
+                                    consumer_secret, access_token, access_token_secret)
     # auth.set_access_token(access_token, access_token_secret)
     print("Defined tokens...")
 
@@ -56,6 +58,34 @@ def createQuoteTweet():
     input('Press any key to exit...')
 
 
+def getUserTimeline():
+    client = tweepy.Client(bearer_token)
+    tweets = client.get_users_tweets(id=USER_ID, tweet_fields=[
+                                     'context_annotations', 'created_at', 'geo'])
+    for tweet in tweets.data:
+        print(tweet)
+
+
+def sampleCreateTweet():
+    client = tweepy.Client(consumer_key=consumer_key,
+                           consumer_secret=consumer_secret,
+                           access_token=access_token,
+                           access_token_secret=access_token_secret)
+
+
+    ct = datetime.datetime.now()
+    # Replace the text with whatever you want to Tweet about
+    parent_tweet = client.create_tweet(text=f'hello world {ct.timestamp()}')
+    print(parent_tweet)
+    print(parent_tweet[0])
+    parent_data = parent_tweet[0]
+    print(parent_data['id'])
+
+    details_tweet = client.create_tweet(text='Check for more https://www.reuters.com/world/europe/ukraine-pledges-sweeping-personnel-changes-allies-jostle-over-tanks-2023-01-23/', in_reply_to_tweet_id=parent_data['id'])
+    print(details_tweet)
+
+
 # sched.add_job(createQuoteTweet, "interval", seconds=10)
 # sched.start()
-createQuoteTweet()
+# createQuoteTweet()
+sampleCreateTweet()
